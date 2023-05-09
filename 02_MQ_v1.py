9e10265b466d1fba80c0d40db04b4bd6a55f9a0c
Sains120 <sainsburys@middleton.school.nz>
2023-05-10 11:28:00 +1200

    changes

--- 02_MQ_v1.py
+++ 02_MQ_v1.py
@@ -20,58 +20,58 @@ answer_one = (input(question_one))
 if answer_one != "tahi":
     print("That's incorrect. Nice try. Answer was tahi. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_two = (input(question_two))
 if answer_two != "rua":
     print("That's incorrect. Nice try. Answer was rua. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_three = (input(question_three))
 if answer_three != "toru":
     print("That's incorrect. Nice try. Answer was toru. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_four = (input(question_four))
 if answer_four != "whā":
     print("That's incorrect. Nice try. Answer was whā. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_five = (input(question_five))
 if answer_five != "rima":
     print("That's incorrect. Nice try. Answer was rima. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_six = (input(question_six))
 if answer_six != "ono":
     print("That's incorrect. Nice try. Answer was ono. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_seven = (input(question_seven))
 if answer_seven != "whitu":
     print("That's incorrect. Nice try. Answer was whitu. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_eight = (input(question_eight))
 if answer_eight != "waru":
     print("That's incorrect. Nice try. Answer was waru. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_nine = (input(question_nine))
 if answer_nine != "iwa":
     print("That's incorrect. Nice try. Answer was iwa. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
 
 answer_ten = (input(question_ten))
 if answer_ten != "tekau":
     print("That's incorrect. Nice try. Answer was tekau. \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's correct! Good job! \n")
--- 02_MQ_v2.py
+++ 02_MQ_v2.py
@@ -3,6 +3,7 @@ Generate the questions and say if wrong or right
 Uppercase and lowercase work
 """
 
+
 # list of questions
 question_one = "What is number one in Māori? "
 question_two = "What is number two in Māori? "
@@ -17,61 +18,63 @@ question_ten = "What is number ten in Māori? "
 
 # questions answers
 answer_one = (input(question_one))
-if answer_one != "tahi" or "Tahi":
-    print("That's incorrect. Nice try. Answer was tahi. \n")
+if answer_one == "tahi" or answer_one == "Tahi":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was tahi. \n")
+
 
 answer_two = (input(question_two))
-if answer_two != "rua":
-    print("That's incorrect. Nice try. Answer was rua. \n")
+if answer_two == "rua" or answer_two == "Rua":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was rua. \n")
 
 answer_three = (input(question_three))
-if answer_three != "toru":
-    print("That's incorrect. Nice try. Answer was toru. \n")
+if answer_three == "toru" or answer_three == "Toru":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was toru. \n")
 
 answer_four = (input(question_four))
-if answer_four != "whā":
-    print("That's incorrect. Nice try. Answer was whā. \n")
+if answer_four == "whā" or answer_four == "Whā" or \
+        answer_four == "wha" or answer_four == "Wha":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was whā. \n")
 
 answer_five = (input(question_five))
-if answer_five != "rima":
-    print("That's incorrect. Nice try. Answer was rima. \n")
+if answer_five == "rima" or answer_five == "Rima":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was rima. \n")
 
 answer_six = (input(question_six))
-if answer_six != "ono":
-    print("That's incorrect. Nice try. Answer was ono. \n")
+if answer_six == "ono" or answer_six == "Ono":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was ono. \n")
 
 answer_seven = (input(question_seven))
-if answer_seven != "whitu":
-    print("That's incorrect. Nice try. Answer was whitu. \n")
+if answer_seven == "whitu" or answer_seven == "Whitu":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was whitu. \n")
 
 answer_eight = (input(question_eight))
-if answer_eight != "waru":
-    print("That's incorrect. Nice try. Answer was waru. \n")
+if answer_eight == "waru" or answer_eight == "Waru":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was waru. \n")
 
 answer_nine = (input(question_nine))
-if answer_nine != "iwa":
-    print("That's incorrect. Nice try. Answer was iwa. \n")
+if answer_nine == "iwa" or answer_nine == "Iwa":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was iwa. \n")
 
 answer_ten = (input(question_ten))
-if answer_ten != "tekau":
-    print("That's incorrect. Nice try. Answer was tekau. \n")
+if answer_ten == "tekau" or answer_ten == "Tekau":
+    print("That's correct! Good job! \n")
 else:
-    print("That correct! Good job! \n")
+    print("That's incorrect. Nice try. Answer was tekau. \n")
