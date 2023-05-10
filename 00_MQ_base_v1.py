a438a5f699afb62b4aaf39328ec0128c732f310c
Sains120 <sainsburys@middleton.school.nz>
2023-05-11 11:44:17 +1200

    changes

--- 00_MQ_base_v1.py
+++ 00_MQ_base_v1.py
@@ -119,7 +119,7 @@ play_again = input("Would you like to play again? ")
 
 if play_again == "yes" or play_again == "Yes" \
         or play_again == "y" or play_again == "Y":
-    print("Questions again")
+    print()
 elif play_again == "no" or play_again == "No"\
         or play_again == "n" or play_again == "N":
     print("*****Thanks for playing the Maori Number Quiz!*****")
